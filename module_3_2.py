f101be5e510947c42ff09089fc6413551c5c8a1a
NightCat-rnd <andrey.chekunov@gmail.com>
2024-10-11 23:19:21 +0300

    Практическое задание 3.2

--- module_3_2.py
+++ module_3_2.py
@@ -42,19 +42,20 @@ send_email('Напоминаю самому себе о вебинаре', 'urba
 Именованные аргументы всегда идут после позиционных.
 '''
 
-def send_email(message, recipient, sender = 'university.help@gmail.com'):
-    domains_ok = {'com', 'ru', 'net'}               # Разрешенные домены
-    recipient.lower()                               # переводим все в нижний регистр
+
+def send_email(message, recipient, sender='university.help@gmail.com'):
+    domains_ok = {'com', 'ru', 'net'}  # Разрешенные домены
+    recipient.lower()  # переводим все в нижний регистр
     sender.lower()
-    sender_domains = sender[-4:].split('.',1)[1]    # определяем домен отправителя (без точки)
-    recipient_domains = recipient[-4:].split('.',1)[1]          # определяем домен получателя
+    sender_domains = sender[-4:].split('.', 1)[1]  # определяем домен отправителя (без точки)
+    recipient_domains = recipient[-4:].split('.', 1)[1]  # определяем домен получателя
 
     if '@' not in list(sender) or '@' not in list(recipient) or \
             sender_domains not in domains_ok or recipient_domains not in domains_ok:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    elif recipient == sender :
+    elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
-    elif sender == 'university.help@gmail.com' :
+    elif sender == 'university.help@gmail.com':
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
     else:
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
