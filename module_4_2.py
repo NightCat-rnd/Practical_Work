965ece06e73946599e381dd0b82c8a2b1c6f05cf
NightCat-rnd <andrey.chekunov@gmail.com>
2024-10-27 18:41:08 +0300

    Практическое задание 4.2

--- module_4_2.py
+++ module_4_2.py
@@ -8,25 +8,11 @@
 3. Вызовите функцию inner_function внутри функции test_function
 4. Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
 '''
-global_a = 'Global A'
 def test_function():
-    global global_a
-    global_a += ' test_function'
-    test_a = ' Test_Function'
     def inner_function():
-        nonlocal test_a
-        global global_a
         str_ = "Я в области видимости функции test_function"
         print(str_)
-        test_a += ' inner_function'
-        global_a += test_a
-        print(locals())
     inner_function()
-    print(locals())
 
-
-for i in range(3):
-    print('Iteration:',i)
-    test_function()
-    print(global_a)
-#inner_function()
+test_function()
+#inner_function() #невозможно вызвать, т.к. вне test_function() такой функции нет
