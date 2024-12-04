319a7390ae6b7cff8b5af92fe9e5a6a711c40b42
NightCat-RND <andrey.chekunov@gmail.com>
2024-12-04 17:41:23 +0300

    Домашняя работа 9.2

--- module_9_2.py
+++ module_9_2.py
@@ -3,14 +3,12 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 first_result = [len(x) for x in first_strings if len(x) >= 5]
-second_result = [(x,y) for x,y in zip(first_strings, second_strings)]
-second_result1 = [(first_strings[x],second_strings[y]) for x in range(len(first_strings)) \
-                  for y in range(len(second_strings)) if x==y]
+second_result = [(first_strings[x],second_strings[y]) for x in range(len(first_strings)) \
+                  for y in range(len(second_strings)) if len(first_strings[x])==len(second_strings[y])]
 third_result = {i:len(i) for i in (first_strings+second_strings) if not len(i)%2}
 
 print(first_result)
 print(second_result)
-print(second_result1)
 print(third_result)
 '''
 Вывод на консоль:
