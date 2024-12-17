07445b5dee6020f9de9b4f417f8d8abb9a39e50b
NightCat-RND <andrey.chekunov@gmail.com>
2024-12-17 20:51:03 +0300

    Домашняя работа 10_1

--- module_10_1.py
+++ module_10_1.py
@@ -11,27 +11,27 @@ def write_words(word_count:int, file_name:str):
             sleep(0.1)
         print(f'Завершилась запись в файл {file_name}')
 
-if __name__ == '__main__':
-    time_start = perf_counter()
-    write_words(10, 'example1.txt')
-    write_words(30, 'example2.txt')
-    write_words(200, 'example3.txt')
-    write_words(100, 'example4.txt')
-    time_end = perf_counter()
-    print(f'Работа потоков {time_end - time_start:0.2f} секунд')
+def main_mono_thread():
+    count_filenames = [(10,'example1.txt'), (30,'example2.txt'), (200,'example3.txt'), (100,'example4.txt')]
+    for count, filenames in count_filenames:
+        write_words(count, filenames)
 
-    time_start = perf_counter()
-    thread1 = threading.Thread(target=write_words, args=(10, 'example5.txt'))
-    thread2 = threading.Thread(target=write_words, args=(30, 'example6.txt'))
-    thread3 = threading.Thread(target=write_words, args=(200, 'example7.txt'))
-    thread4 = threading.Thread(target=write_words, args=(100, 'example8.txt'))
-    thread1.start()
-    thread2.start()
-    thread3.start()
-    thread4.start()
-    thread1.join()
-    thread2.join()
-    thread3.join()
-    thread4.join()
-    time_end = perf_counter()
-    print(f'Работа потоков {time_end - time_start:0.2f} секунд')
+def main_multi_thread():
+    count_filenames = [(10, 'example5.txt'), (30, 'example6.txt'), (200, 'example7.txt'), (100, 'example8.txt')]
+    threads = [threading.Thread(target=write_words, args=(count, filename)) for count, filename in count_filenames]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == '__main__':
+    start_time = perf_counter()
+    main_mono_thread()
+    end_time = perf_counter()
+    print(f'Работа потоков {end_time-start_time:0.2f} секунд')
+
+    start_time = perf_counter()
+    main_multi_thread()
+    end_time = perf_counter()
+    print(f'Работа потоков {end_time - start_time:0.2f} секунд')
